35ef472ea316fd3525efaad8467ee2d583e12110
VASPerera <avishka1999perera@gmail.com>
2025-10-30 14:30:06 +0530

    mogodb established. created models and middleware.

---
+++ backend/models/Transaction.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const TransactionSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  text: {
+    type: String,
+    trim: true,
+    required: [true, 'Please add some text']
+  },
+  amount: {
+    type: Number,
+    required: [true, 'Please add a positive or negative number']
+  },
+  type: {
+    type: String,
+    enum: ['income', 'expense'],
+    required: true
+  },
+  category: {
+    type: String,
+    trim: true,
+    default: 'General'
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Transaction', TransactionSchema);
